6dfa88b0b876f7a92690ffd39d03fd5f74c9bc3d
skshrestha <skshrestha@deerwalk.com>
2016-01-22 18:17:24 +0545

    added login properties.

--- src/main/java/com/pcjoshi/stmr/Server.java
+++ src/main/java/com/pcjoshi/stmr/Server.java
@@ -1,13 +1,20 @@
 package com.pcjoshi.stmr;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
 
 /**
  * Created by pcjoshi on 1/22/16. Email: pcjoshi@deerwalk.com
  */
-public class Server extends AbstractVerticle{
-    public Server(){
-        System.out.println("i am in ............");
-    }
+public class Server extends AbstractVerticle {
 
+    Logger log = LoggerFactory.getLogger("io.vertx.core.logging.Logger logger");
+
+    @Override
+    public void start() {
+
+        log.info("... well come to stream server ...");
+
+    }
 }
