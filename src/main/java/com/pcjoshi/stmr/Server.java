7d36997ab0d103ef150718b0a68b43463e85ffb3
pcjoshi <pcjoshi@deerwalk.com>
2016-01-22 17:30:15 +0545

    Initial commit

---
+++ src/main/java/com/pcjoshi/stmr/Server.java
@@ -0,0 +1,10 @@
+package com.pcjoshi.stmr;
+
+import io.vertx.core.AbstractVerticle;
+
+/**
+ * Created by pcjoshi on 1/22/16. Email: pcjoshi@deerwalk.com
+ */
+public class Server extends AbstractVerticle{
+
+}
